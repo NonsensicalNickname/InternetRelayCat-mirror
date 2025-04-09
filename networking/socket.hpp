332caae5be021ff468543daa4a39617ae55c6f5a
nonsensicalnickname <nonsensical_nickname@protonmail.com>
2025-04-09 22:16:58 +1000

    basic socket-ry using handcoded addrinfo

---
+++ networking/socket.hpp
@@ -0,0 +1,27 @@
+#ifndef Socket_hpp
+#define Socket_hpp
+
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <iostream>
+
+namespace catsock {
+  class Socket {
+
+    private: 
+      struct sockaddr_in address;
+      int sock_fdesc;
+      int connection;
+    public:
+      Socket(int family, int service, int protocol, int port, u_long interface);
+      void check_connection(int val);
+      struct sockaddr_in get_address();
+      int get_sock_fdesc();
+      int get_connection();
+  };
+}
+
+#endif
